d109b82b07f0c2555aae9c415ba585d10e5c2d98
Semih <putnamsemih@gmail.com>
2022-05-11 20:29:55 -0400

    Day 2 Task 2 find Element by link text

---
+++ src/test/java/com/cydeo/test/day2_locators/T2_LinkTextPractice.java
@@ -0,0 +1,61 @@
+package com.cydeo.test.day2_locators;
+
+/*
+TC #2: Back and forth navigation
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com
+3- Click to A/B Testing from top of the list.
+4- Verify title is:
+  Expected: No A/B Test
+5- Go back to home page by using the .back();
+6- Verify title equals:
+  Expected: Practice
+ */
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T2_LinkTextPractice {
+
+    public static void main(String[] args) throws InterruptedException { // Exception to Thread.sleep(1000) method
+
+        WebDriverManager.chromedriver().setup(); // WEBDRIVER SETTING UP CHROMEDRIVER
+
+        WebDriver driver = new ChromeDriver(); // CREATING A NEW CHROMEDRIVER OBJECT WITH WEBDRIVER REFERENCE
+
+        driver.manage().window().maximize(); // MAXIMIZES THE SCREEN
+
+        driver.get("https://practice.cydeo.com"); // DRIVER GETS URL
+
+        Thread.sleep(1000); // SLOWS DOWN THE DRIVER FOR 1000 ms
+
+        driver.findElement(By.linkText("A/B Testing")).click(); // LOCATES BY THE HYPERLINKED ELEMENT CONTENT AND CLICKS IT
+
+        String actualTitle = driver.getTitle(); // STORES TITLE AS STRING VARIABLE
+
+        String expectedTitle = "No A/B Test"; // DECLARE AN ASSIGN expectedTitle VARIABLE
+
+        System.out.println((actualTitle.equals(expectedTitle)) ? "A/B Testing Title Verification Passed" : "A/B Testing Title Verification Failed"); // TERNARY WHICH CHECKS IF ACTUAL CONTAINS EXPECTED VALUE
+
+        Thread.sleep(1000); // SLOWS DOWN THE DRIVER FOR 1000 ms
+
+        driver.navigate().back(); // NAVIGATES THE PAGE BACK BY ONE
+
+        String actualTitle2 = driver.getTitle(); // STORES TITLE AS STRING VARIABLE
+
+        String expectedTitle2 = "Practice"; // DECLARE AN ASSIGN expectedTitle VARIABLE
+
+        System.out.println((actualTitle2.equals(expectedTitle2)) ? "Home Page Title Verification Passed" : "Home Page Title Verification Failed");
+        // TERNARY WHICH CHECKS IF ACTUAL CONTAINS EXPECTED VALUE
+
+        Thread.sleep(1000); // SLOWS DOWN THE DRIVER FOR 1000 ms
+
+        driver.quit(); // CLOSES THE CHROME DRIVER
+
+
+
+
+    }
+}
