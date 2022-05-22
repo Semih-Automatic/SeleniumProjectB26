f57be9bcaa3685f4cd0f8973329189bf1ad4e52c
Semih <putnamsemih@gmail.com>
2022-05-22 15:11:11 -0400

    Task 3 get Attribute And Css

--- src/test/java/com/cydeo/test/day3_Css_getText_getAttribute/T3_getAttribute_Css.java
+++ src/test/java/com/cydeo/test/day3_Css_getText_getAttribute/T3_getAttribute_Css.java
@@ -21,16 +21,25 @@ public class T3_getAttribute_Css {
         //2- Go to: https://login1.nextbasecrm.com/
         driver.get("https://login1.nextbasecrm.com/");
 
-        //3- Verify “Log in” button text is as expected:
-        //Expected: Log In
-        WebElement logIn = driver.findElement(By.cssSelector("input[type='submit']"));
-        String expectedText = "Log In";
-        String actualText = logIn.getAttribute("value");
+        //        3- Verify “Log in” button text is as expected:
+//        Expected: Log In
 
-        if(actualText.equals(expectedText)){
-            System.out.println("Button Text Verification Passed");
-        } else {
-            System.out.println("Button Text Verification Failed");
+        // Located using type attribute
+        // WebElement loginButton = driver.findElement(By.cssSelector("input[type='submit']"));
+
+        // Located using value attribute
+        // WebElement loginButton = driver.findElement(By.cssSelector("input[value='Log In']"));
+
+        // Located using class attribute
+        WebElement loginButton = driver.findElement(By.cssSelector("input[class='login-btn']"));
+
+        String expectedBtnText = "Log In";
+        String actualBtnText = loginButton.getAttribute("value");
+
+        if(actualBtnText.equals(expectedBtnText)){
+            System.out.println("Log In button text verification passed!");
+        }else{
+            System.out.println("Log In button text verification failed!");
         }
 
         driver.quit();
