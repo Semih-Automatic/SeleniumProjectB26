8dcd18ca4f344976e26f28b690572439fce47bc0
Semih <putnamsemih@gmail.com>
2022-05-25 22:01:40 -0400

    cssSelector getAttribute

---
+++ src/test/java/com/cydeo/test/day3_Css_getText_getAttribute/T4_cssSelector_getText.java
@@ -0,0 +1,34 @@
+package com.cydeo.test.day3_Css_getText_getAttribute;
+
+import com.cydeo.test.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T4_cssSelector_getText {
+
+    public static void main(String[] args) {
+        //TC #4: NextBaseCRM, locators, getText(), getAttribute() practice
+        //1- Open a chrome browser
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        //2- Go to: https://login1.nextbasecrm.com/?forgot_password=yes
+        driver.get("https://login1.nextbasecrm.com/?forgot_password=yes");
+
+        // 3- Verify “Reset password” button text is as expected:
+        //Expected: Reset password
+        WebElement resetPassword = driver.findElement(By.cssSelector("button.login-btn"));
+        String actualText = resetPassword.getAttribute("value");
+        String expectedText = "Reset password";
+
+        if(actualText.equals(expectedText)){
+            System.out.println("Reset Password Text Verification Passed!");
+        } else {
+            System.out.println("Reset Password Text Verification Failed!");
+        }
+
+        driver.quit();
+
+    }
+}
